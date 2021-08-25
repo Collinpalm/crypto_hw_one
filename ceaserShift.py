cf2722c99310564ae441b9157551304d616d180e
Collin Palm <collinpalm@gmail.com>
2021-08-25 11:51:44 -0400

    brute force caeser cipher

--- ceaserShift.py
+++ ceaserShift.py
@@ -1,6 +1,11 @@
+ALPHABET = "abcdefghijklmnopqrstuvwxyz"
 
 def main(ciphertext):
-    for element in ciphertext:
-        character = ord(element)
-        character = character - 64
-        
+    for key in range(0, len(ALPHABET)):
+        for element in ciphertext:
+            character = ord(element)
+            character = character - 64
+            character = (character + key) % 26
+            print(chr(character))
+            character = ''
+        print("\n")
