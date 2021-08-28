4e720161a0727a8cf688c200449cd5a32e9016a0
Collin Palm <collinpalm@gmail.com>
2021-08-28 17:10:01 -0400

    more renames

---
+++ affineEncrypt.py
@@ -0,0 +1,20 @@
+import sys
+
+ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+
+def encrypt():
+    ciphertext = sys.argv[1]
+    character = []
+    for element in range(0,len(ciphertext)):
+        character.append(ord(ciphertext[element]))
+        character[i] = character[i] - 97
+        i = i+1
+    
+
+
+
+
+
+    
+if __name__ == '__main__':
+    encrypt()
