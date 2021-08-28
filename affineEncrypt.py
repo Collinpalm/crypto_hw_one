3f73ff637b83d1ce13e9f89738991c3efc4cbc13
Collin Palm <collinpalm@gmail.com>
2021-08-28 17:16:05 -0400

    encryption

--- affineEncrypt.py
+++ affineEncrypt.py
@@ -3,14 +3,23 @@ import sys
 ALPHABET = "abcdefghijklmnopqrstuvwxyz"
 
 def encrypt():
-    ciphertext = sys.argv[1]
+    plaintext = sys.argv[1]
+    a = sys.argv[2]
+    b = sys.argv[3]
     character = []
-    for element in range(0,len(ciphertext)):
-        character.append(ord(ciphertext[element]))
+    cipherText = ""
+    for element in range(0,len(plaintext)):
+        character.append(ord(plaintext[element]))
         character[i] = character[i] - 97
+        character[i] = ((a * character[i]) + b) % 26
+        character[i] = character[i] + 97
+        character[i] = chr(character[i])
         i = i+1
-    
-    
+    for i in range(0, len(character)):
+        cipherText += character[i]
+
+    print("Encrypted Text")
+    print(cipherText)
 
 
 
